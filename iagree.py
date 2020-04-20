d70df99af753863d47ccd0f6fd104d30c678af0c
circumvention <47408635+circumvention@users.noreply.github.com>
2020-04-20 10:54:31 -0700

    Add files via upload

---
+++ iagree.py
@@ -0,0 +1,18 @@
+from tkinter import *
+import urllib.request
+import json
+
+url = 'https://www.googleapis.com/youtube/v3/videos?part=statistics&id=gnPPgX1p5lc&key=AIzaSyDzexaJCbzger34TtAeWgkVebossSpU_XI'
+key = 'AIzaSyDzexaJCbzger34TtAeWgkVebossSpU_XI'
+
+def VideoData():
+	data = urllib.request.urlopen(url).read()
+	
+	viewCount = str(json.loads(data)['items'][0]['statistics']['viewCount'])
+	print(viewCount)
+	
+
+VideoData()
+	
+	
+	
