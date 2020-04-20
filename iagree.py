81ba41cb4fba71ded237c027f5a1fa59f9e840ed
circumvention <47408635+circumvention@users.noreply.github.com>
2020-04-20 11:08:51 -0700

    Update iagree.py

--- iagree.py
+++ iagree.py
@@ -1,8 +1,10 @@
+from tkinter import *
 import urllib.request
-import json
+import json 
 
 url = 'https://www.googleapis.com/youtube/v3/videos?part=statistics&id=gnPPgX1p5lc&key=AIzaSyDzexaJCbzger34TtAeWgkVebossSpU_XI'
 key = 'AIzaSyDzexaJCbzger34TtAeWgkVebossSpU_XI'
+SnipURL = 'https://www.googleapis.com/youtube/v3/videos?part=snippet&id=gnPPgX1p5lc&key=AIzaSyDzexaJCbzger34TtAeWgkVebossSpU_XI'
 
 def VideoData():
 	data = urllib.request.urlopen(url).read()
@@ -10,8 +12,12 @@ def VideoData():
 	viewCount = str(json.loads(data)['items'][0]['statistics']['viewCount'])
 	print(viewCount)
 	
+def titleData():
+	data = urllib.request.urlopen(SnipURL).read()
+	
+	title = str(json.loads(data)['items'][0]['snippet']['title'])
+	print(title)
+
 
 VideoData()
-	
-	
-	
+titleData()
