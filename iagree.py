4dbe14155cceeff4a78b12f43a098a1362c47e2c
circumvention <47408635+circumvention@users.noreply.github.com>
2020-04-20 12:05:24 -0700

    Update iagree.py

--- iagree.py
+++ iagree.py
@@ -4,6 +4,12 @@ import googleapiclient.errors
 import urllib.request
 import json 
 
+import gdata.youtube
+import gdata.youtube.service
+
+yt_service = gdata.youtube.service.YouTubeService()
+
+
 url = 'https://www.googleapis.com/youtube/v3/videos?part=statistics&id=gnPPgX1p5lc&key=AIzaSyBpJrEkp6QfLqy1ZPeLeCXLN_eIW_Ir7_w'
 key = 'AIzaSyBpJrEkp6QfLqy1ZPeLeCXLN_eIW_Ir7_w'
 SnipURL = 'https://www.googleapis.com/youtube/v3/videos?part=snippet&id=gnPPgX1p5lc&key=AIzaSyBpJrEkp6QfLqy1ZPeLeCXLN_eIW_Ir7_w'
@@ -23,14 +29,17 @@ title = str(json.loads(data)['items'][0]['snippet']['title'])
 print(title)
 
 request = youtube.videos().update(
+        part="id,snippet",
         body={
           "id": "gnPPgX1p5lc",
           "snippet": {
-            "title": 'if this video has ' + viewCount +  'views, youre cool'
+            "title": "test",
+            "categoryId": "23"
           }
         }
     )
 response = request.execute()
+print(response)
 
 print(response)
 VideoData()
