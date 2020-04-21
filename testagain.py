c8c9990928eb99b94365e0808c270def8e7aaa53
circumvention <47408635+circumvention@users.noreply.github.com>
2020-04-20 19:42:10 -0700

    Update testagain.py

--- testagain.py
+++ testagain.py
@@ -1,44 +1,38 @@
-# -*- coding: utf-8 -*-
-
-# Sample Python code for youtube.videos.update
-# See instructions for running these code samples locally:
-# https://developers.google.com/explorer-help/guides/code_samples#python
-
-import os
-
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
+import urllib.request
+from pyyoutube import Api
 
-scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
 
-def main():
-    # Disable OAuthlib's HTTPS verification when running locally.
-    # *DO NOT* leave this option enabled in production.
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
-    api_service_name = "youtube"
-    api_version = "v3"
-    client_secrets_file = "YOUR_CLIENT_SECRET_FILE.json"
 
-    # Get credentials and create an API client
-    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-        client_secrets_file, scopes)
-    credentials = flow.run_console()
-    youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, credentials=credentials)
 
-    request = youtube.videos().update(
+url = 'https://www.googleapis.com/youtube/v3/videos?part=statistics&id=gnPPgX1p5lc&key=AIzaSyBpJrEkp6QfLqy1ZPeLeCXLN_eIW_Ir7_w'
+key = 'AIzaSyBpJrEkp6QfLqy1ZPeLeCXLN_eIW_Ir7_w'
+SnipURL = 'https://www.googleapis.com/youtube/v3/videos?part=snippet&id=gnPPgX1p5lc&key=AIzaSyBpJrEkp6QfLqy1ZPeLeCXLN_eIW_Ir7_w'
+
+api_service_name = "youtube"
+api_version = "v3"
+
+api = Api(api_key=key )
+
+
+#ViewCount
+data = urllib.request.urlopen(url).read()
+viewCount = str(json.loads(data)['items'][0]['statistics']['viewCount'])
+print(viewCount)
+
+
+request = youtube.videos().update(
+        part="id,snippet",
         body={
           "id": "gnPPgX1p5lc",
           "snippet": {
-            "title": "if this video has views, you're cool"
+            "title": "test1",
+            "categoryId": "23"
           }
         }
     )
-    response = request.execute()
-
-    print(response)
-
-if __name__ == "__main__":
-    main()
+response = request.execute()
+print(response)
