9eea152239b2535af5467edb6a078aef7113e0bf
circumvention <47408635+circumvention@users.noreply.github.com>
2020-04-21 11:33:41 -0700

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,8 +1,4 @@
-# -*- coding: utf-8 -*-
 
-# Sample Python code for youtube.channels.list
-# See instructions for running these code samples locally:
-# https://developers.google.com/explorer-help/guides/code_samples#python
 
 import os
 
@@ -20,27 +16,32 @@ def main():
     api_service_name = "youtube"
     api_version = "v3"
     client_secrets_file = "YOUR_CLIENT.json"
-
+    amount  = 0
     # Get credentials and create an API client
     flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
         client_secrets_file, scopes)
     credentials = flow.run_console()
     youtube = googleapiclient.discovery.build(
         api_service_name, api_version, credentials=credentials)
+    
+    data = urllib.request.urlopen(url).read()
+viewCount = str(json.loads(data)['items'][0]['statistics']['viewCount'])
 
+  while True:  
     request = youtube.videos().update(
         part="snippet",
         body={
           "id": "gnPPgX1p5lc",
           "snippet": {
-            "title": "Test again lul",
+            "title": "if this video has " + str(viewCount) + "you're cool",
             "categoryId": "23"
           }
         }
     )
     response = request.execute()
+    amount += 1
 
-    print(response)
+    print('execute succesful' + amount)
 
 if __name__ == "__main__":
     main()
