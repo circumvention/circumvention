1cd25411a84ce02d42a876e8c6a41c18bdeabaef
circumvention <47408635+circumvention@users.noreply.github.com>
2020-04-24 11:39:01 -0700

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,20 +1,16 @@
-import os
-import time
-import json
-
-import google_auth_oauthlib.flow
-import googleapiclient.discovery
-import googleapiclient.errors
-import urllib
-import sys
-
-
-
 scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
 
 def main():
     # Disable OAuthlib's HTTPS verification when running locally.
     # *DO NOT* leave this option enabled in production.
+    import os
+    import time
+    import json
+    import google_auth_oauthlib.flow
+    import googleapiclient.discovery
+    import googleapiclient.errors
+    import urllib
+    import sys
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
     api_service_name = "youtube"
